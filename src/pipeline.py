801ea1391d3bd5c75fc23266a2ab6bc85fc1a058
GurgelAmaral <monsieurlambertz@gmail.com>
2025-06-21 19:56:09 -0300

    criação do pipeline do modelo

---
+++ src/pipeline.py
@@ -0,0 +1,28 @@
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.ensemble import RandomForestClassifier
+
+def build_model_pipeline(cat_cols=None, n_estimators=200, max_depth=None):
+    #verificação se cat_cols está nulo
+    if cat_cols is None:
+        raise ValueError('cat_cols deve ser preenchido')
+
+    #criação de column_transformer e automação do processo de ordinal encoding paraa variáveis categóricas
+    transf = ColumnTransformer(
+        transformers=[
+            ('cat_trans', OrdinalEncoder(), cat_cols)
+        ],
+
+        #variáveis numéricas não passam por encoding e continuam como estão
+        remainder='passthrough'
+    )
+
+    #pipeline do modelo 
+    model_pipeline = Pipeline([
+        ('prep', transf),
+        ('classifier', RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth))
+    ])
+
+    return model_pipeline
+
