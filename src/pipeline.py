18c1a3398168c36e23d5a42f33ddc96a5b8cb838
GurgelAmaral <monsieurlambertz@gmail.com>
2025-06-23 22:32:25 -0300

    correção de pipeline para casos numéricos e casos vazios

--- src/pipeline.py
+++ src/pipeline.py
@@ -7,17 +7,43 @@ from sklearn.preprocessing import StandardScaler
 def build_model_pipeline(cat_cols=None, num_cols=None, n_estimators=200, max_depth=None):
     #verificação se cat_cols está nulo
     if cat_cols is None or num_cols is None:
-        raise ValueError('cat_cols e num_cols devem ser preenchidos deve ser preenchido')
+        raise ValueError('cat_cols e num_cols devem ser preenchidos como parâmetros da função build_model_pipeline')
 
-    #criação de column_transformer e automação do processo de ordinal encoding paraa variáveis categóricas
-    transf = ColumnTransformer(
-        transformers=[
-            ('cat_trans', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1), cat_cols),
-            ('num_trans', StandardScaler(), num_cols)
-        ],
+    #criação de column_transformer e automação do processo de ordinal encoding para variáveis categóricas
+    #caso se ambas as colunas forem vazias
+    if not cat_cols and not num_cols:
+        raise ValueError('não há colunas para transformar')
     
-        remainder='drop'
-    )
+    #se cat_cols for vazia
+    elif not cat_cols:
+        transf = ColumnTransformer(
+            transformers=[
+                ('num_trans', StandardScaler(), num_cols)
+            ],
+    
+            remainder='drop'
+        )
+
+    #se num_cols for vazia
+    elif not num_cols:
+        transf = ColumnTransformer(
+            transformers=[
+                ('cat_trans', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1), cat_cols)
+            ],
+    
+            remainder='drop'
+        )
+        
+    #caso contrário
+    else:
+        transf = ColumnTransformer(
+            transformers=[
+                ('cat_trans', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1), cat_cols),
+                ('num_trans', StandardScaler(), num_cols)
+            ],
+    
+            remainder='drop'
+        )
 
     #pipeline do modelo 
     model_pipeline = Pipeline([
