96ce273948d8ee5ad8028b7799618b90afd4bdbe
GurgelAmaral <monsieurlambertz@gmail.com>
2025-06-23 21:53:56 -0300

    modificação de transformer e features.py para nums

--- src/features.py
+++ src/features.py
@@ -3,3 +3,5 @@
 def get_cat_features(df):
     return df.select_dtypes(include='object').columns.to_list()
 
+def get_num_features(df):
+    return df.select_dtypes(include='number').columns.to_list()
--- src/pipeline.py
+++ src/pipeline.py
@@ -2,20 +2,21 @@ from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import StandardScaler
 
-def build_model_pipeline(cat_cols=None, n_estimators=200, max_depth=None):
+def build_model_pipeline(cat_cols=None, num_cols=None, n_estimators=200, max_depth=None):
     #verificação se cat_cols está nulo
-    if cat_cols is None:
-        raise ValueError('cat_cols deve ser preenchido')
+    if cat_cols is None or num_cols is None:
+        raise ValueError('cat_cols e num_cols devem ser preenchidos deve ser preenchido')
 
     #criação de column_transformer e automação do processo de ordinal encoding paraa variáveis categóricas
     transf = ColumnTransformer(
         transformers=[
-            ('cat_trans', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1), cat_cols)
+            ('cat_trans', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1), cat_cols),
+            ('num_trans', StandardScaler(), num_cols)
         ],
-
-        #variáveis numéricas não passam por encoding e continuam como estão
-        remainder='passthrough'
+    
+        remainder='drop'
     )
 
     #pipeline do modelo 
