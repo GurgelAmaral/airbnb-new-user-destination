d4b926522536fee0d091bcf92fe8c6d353b0172e
GurgelAmaral <monsieurlambertz@gmail.com>
2025-06-24 12:49:37 -0300

    correção da implementação do mlp no pipeline.py

--- src/pipeline.py
+++ src/pipeline.py
@@ -5,12 +5,19 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 
-def build_model_pipeline(cat_cols=None, num_cols=None, n_estimators=200, max_depth=None):
+def build_model_pipeline(cat_cols=None, 
+                         num_cols=None, 
+                         hidden_layers_set=(40,20),
+                         activation_func='relu',
+                         learning_rate_init=0.001,
+                         solve_method='sgd',
+                         epochs=300):
+    
     #verificação se cat_cols está nulo
     if cat_cols is None or num_cols is None:
         raise ValueError('cat_cols e num_cols devem ser preenchidos como parâmetros da função build_model_pipeline')
 
-    #criação de column_transformer e automação do processo de ordinal encoding para variáveis categóricas
+    #criação de column_transformer e automação do processo de ordinal encoding para variáveis categóricas e StdScaler -> num
     #caso se ambas as colunas forem vazias
     if not cat_cols and not num_cols:
         raise ValueError('não há colunas para transformar')
@@ -50,11 +57,11 @@ def build_model_pipeline(cat_cols=None, num_cols=None, n_estimators=200, max_dep
     model_pipeline = Pipeline([
         ('prep', transf),
         ('classifier', MLPClassifier(
-            hidden_layer_sizes=(40,20), 
-            activation='relu', 
-            learning_rate_init=0.001, 
-            solver='sgd',
-            max_iter=300))
+            hidden_layer_sizes=hidden_layers_set, 
+            activation=activation_func, 
+            learning_rate_init=learning_rate_init, 
+            solver=solve_method,
+            max_iter=epochs))
     ])
 
     return model_pipeline
