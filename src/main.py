9429ea5d4538df0a9736204ce081c6b138de961d
GurgelAmaral <monsieurlambertz@gmail.com>
2025-06-29 17:40:42 -0300

    adição de exportação do modelo com joblib

--- src/main.py
+++ src/main.py
@@ -23,9 +23,9 @@ y = pd.read_csv('y_train_resampled.csv', index_col=0)
 cat_cols = get_cat_features(x)
 
 #separação para utilização de apenas 15% do dataset (15% de ~1.23e+6 linhas)
-x_train, _, y_train, _ = train_test_split(x, y, train_size=0.15, stratify=y)
+x_fraction, _, y_fraction, _ = train_test_split(x, y, train_size=0.15, stratify=y)
 #separação de treino e teste para uso no modelo
-x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.2, stratify=y_train)
+x_train, x_test, y_train, y_test = train_test_split(x_fraction, y_fraction, test_size=0.2, stratify=y_fraction)
 
 #instância do modelo pelo seu pipeline
 model = build_model_pipeline(cat_cols=cat_cols)
@@ -36,3 +36,10 @@ best_model = get_best_estimators(model=model, x_train=x_train, y_train=y_train, 
 #lista de predições do melhor modelo para uso do cálculo do acurácia
 pred_list = best_model.predict(x_test)
 print(f'acurácia: {evaluate_model(y_test, pred_list)}')
+
+#salvar e exportar melhor modelo já treinado com joblib
+try:
+    jb.dump(best_model, filename='best_model_trained.joblib')
+    print('Modelo salvo e exportado como "best_model_trained.joblib"')
+except Exception as e:
+    print(f'Algo deu errado durante o saving e exportação do modelo. Rode o código de treino novamente | {e}')
